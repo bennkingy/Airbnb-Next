faa2a9cd7b5cd7117aa25ebaa52ee5ce504337e1
Wrldhorses <wrldhorses@gmail.com>
2023-06-17 21:12:20 +0100

    gmap now flys to the location

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,7 +1,6 @@
-'use client';
-
 import L from 'leaflet';
-import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import React, { useEffect, useRef } from 'react';
+import { MapContainer, Marker, TileLayer, useMap } from 'react-leaflet';
 
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
@@ -25,14 +24,36 @@ const attribution =
   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
 const Map: React.FC<MapProps> = ({ center }) => {
+  const mapRef = useRef<L.Map | null>(null);
+
+  useEffect(() => {
+    if (mapRef.current && center) {
+      const map = mapRef.current;
+      map.flyTo(center as L.LatLngExpression);
+    }
+  }, [center]);
+
+  const FlyToLocation = () => {
+    const map = useMap();
+    if (center) {
+      map.flyTo(center as L.LatLngExpression);
+    }
+    return null;
+  };
+
   return (
     <MapContainer
       center={(center as L.LatLngExpression) || [51, -0.09]}
       zoom={center ? 4 : 2}
       scrollWheelZoom={false}
       className='h-[35vh] rounded-lg'
+      // @ts-ignore
+      whenCreated={(map) => {
+        mapRef.current = map;
+      }}
     >
       <TileLayer url={url} attribution={attribution} />
+      <FlyToLocation />
       {center && <Marker position={center as L.LatLngExpression} />}
     </MapContainer>
   );
